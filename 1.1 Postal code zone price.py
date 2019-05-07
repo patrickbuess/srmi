8c3a4cae598f03d7fa3896de108a43ac91fdcefb
RubenB1 <RubenB1@github.com>
2019-05-07 11:16:24 +0200

    Ruben Burdin draft

--- 1.1 Postal code zone price.py
+++ 1.1 Postal code zone price.py
@@ -250,13 +250,96 @@ class avgPc:
 
            return (plt.show())
 
+   def weather(self,postalCode):
+       list = []
+       postalCodeList = []
+       postalCode = postalCode
+       varx = 'price'
+       if self.dbOperations is None:
+           self.dbOperations = DBOperations.getDB()
+       self.dbOperations.getConnection()
+       #FIRST, collect the data from the varx (which can be defined as a variable for replicability of the code). This will correspond to one of the axis in our 2-dimensional chart.
+       try:
+           with DBOperations.connection.cursor() as cursor:
+               # Select from sql the list of postal codes that are from the same city of the one we would like to look at, because some big cities are divided in more postal codes while being the same city.
+               sql = "SELECT postalCode FROM `postalCodes` WHERE city IN (SELECT city FROM `postalCodes` WHERE postalCode = {})".format(
+                   postalCode)
+               cursor.execute(sql)
+               data = cursor.fetchall()
+               for p in data:
+                   if p['postalCode'] is not None:
+                       c = p['postalCode']
+                       postalCodeList.append(c)
+               print(postalCodeList)
+
+               #Determine if there are one or many postal codes that are in the same city or the city targeted by the user has only 1 postal code attributed.
+               if len(postalCodeList) is 1:
+                   # Select now from the Database the desired data in the case there is only one postal code for the targeted city (because the SQL query syntax changes if there are one or many)
+                   postalCodeList = postalCode
+                   sql = "SELECT {},{} FROM listingDetails WHERE postalCode = {} AND category IN ('Apartment','Furnished apartment','Attic apartment','Maisonette','Loft','Penthouse','Terraced Apartment','Terraced condo')".format(
+                       varx, varx2, postalCodeList)
+                   cursor.execute(sql)
+                   data = cursor.fetchall()
+
+               else:
+                   #Select now from the Database the desired data
+                   # Transform the list of postal codes in tuples so that the next SQL query is ready to work
+                   postalCodeList = tuple(postalCodeList)
+                   sql = "SELECT {},{} FROM listingDetails WHERE postalCode IN {} AND category IN ('Apartment','Furnished apartment','Attic apartment','Maisonette','Loft','Penthouse','Terraced Apartment','Terraced condo')".format(
+                       varx, varx2, postalCodeList)
+                   cursor.execute(sql)
+                   data = cursor.fetchall()
+
+               #Creating 2 lists which define the two axis of the chart
+               for p in data:
+                   if p[varx] is not None and p[varx] not in [0, 1] and p[varx2] is not None and p[varx2] not in [0, 1]:
+                       c = p[varx]
+                       list.append(c)
+                       d = p[varx2]
+                       lista.append(d)
+               print(len(list))
+
+               #Create the 2 list that we will fill with some extreme-values-filtered data so that our plots are representing the reality of the average market and not the extreme cases (that we want to exclude here from our data)
+               axis1 = []
+               axis2 = []
+               #Defining the standard deviation for both sets of data we have at hand (sd for axis1 and sda for axis2)
+               elements = numpy.array(list)
+               mean = numpy.mean(elements, axis=0)
+               sd = numpy.std(elements, axis=0)
+
+               elements = numpy.array(lista)
+               mean = numpy.mean(elements, axis=0)
+               sda = numpy.std(elements, axis=0)
+
+               # Let's define the 2 axis and filter the extreme values from the axis1 and then the extreme values from the axis 2 to obtain a bidimentional coherent plot.
+               #Here we want to exclude outlyers from the dataset by using standard deviations (2 standard deviations here)
+               for x in range(1,len(list)):
+                   if list[x]> (mean - 2 * sd) and list[x]< mean + 2 * sd:
+                       if lista[x]> (mean - 2 * sda) and lista[x]< mean + 2 * sda:
+                           axis1.append(list[x])
+                           axis2.append(lista[x])
+               # axis1 is the first axis we define with the varx variable
+               # axis2 is the first axis we define with the varx variable
+               print(len(axis1))
+
+       finally:
+           cursor.close()
+           plt.scatter(axis1, axis2, s=10, color='#2d85cb')
+           plt.title(varx + " in relation to " + varx2+" at "+ str(postalCode))
+           plt.xlabel(varx)
+           plt.ylabel(varx2)
+
+           return (plt.show())
+
+
+
 #postalCodes = avgPc(DBOperations("kezenihi_srmidb"))
 #postalCodes.distrib(1000, 'size')
 
 postalCodes = avgPc(DBOperations("kezenihi_srmidb"))
 
 #pc = postalCodes.average(9000, 'price', 'size')
-pc = postalCodes.priceMeter(9000)
+pc = postalCodes.priceMeter(1008)
 
 # --------------------------------------------------
 # In this section we create all the geographical map visualization that are necessary to satisfy our customers' needs.
